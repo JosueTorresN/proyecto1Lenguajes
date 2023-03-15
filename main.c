c5ba7dedae242b9dd4a7677f762c7d3bdccba8d3
tim-scarlith <anthony00052023@gmail.com>
2023-03-15 00:13:57 -0600

    commit

--- main.c
+++ main.c
@@ -6,11 +6,34 @@
 
 void menuSelect()
 {
-    int option;
-    printf("Selecione el tipo de menu que desea utilizar: \n");
-    printf("1. Menu Operativo \n");
-    printf("2. Menu Administrativo \n");
-    printf("3. Salir \n");
+    int bandera = 1;
+    do {
+        int option;
+        printf("Selecione el tipo de menu que desea utilizar: \n");
+        printf("1. Menu Operativo \n");
+        printf("2. Menu Administrativo \n");
+        printf("3. Salir \n");
+        scanf("%d", &option);
+        switch (option)
+        {
+        case 1:
+            menuOperativo();
+            break;
+        case 2:
+            // menuAdministrativo();
+            break;
+        case 3:
+            printf("Saliendo del programa... \n");
+            bandera = 0;
+            break;
+        default:    
+            printf("Opcion no valida, intente de nuevo \n");
+            menuSelect();
+            break;
+        }
+    } while (bandera);
+
+}
 
 int main(int argc, char *argv[])
 {
@@ -18,8 +41,9 @@ int main(int argc, char *argv[])
     // SitioEventos* sitioEventos = NULL;
     // sitioEventos = agregarSitioEventos(sitioEventos ,crearSitioEventos("hola", "hala", "holo", "illo"));
     // imprimirSitioEventos(sitioEventos);
-    imprimirSitioEventos(getSitioEventos());
+    // imprimirSitioEventos(getSitioEventos());
     // getSitioEventos();
     // menuOperativo();
+    menuSelect();
     return 0;
 }
